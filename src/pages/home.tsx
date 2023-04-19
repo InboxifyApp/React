6b55106ba32bd1ab1090a08696ca73568bb48001
Alaa Barka <tmakaveli643@gmail.com>
2023-04-19 04:50:15 +0100

    setting up and coming soon animation

---
+++ src/pages/home.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+
+const Home = () =>{
+    return (
+        <div className='w-full h-screen bg-[#1e1e20] flex items-center justify-center anim'>
+            <p className='text-[#962134] font-bold text-5xl homeTxt'>
+                Coming Soon...
+            </p>
+        </div>
+    )
+}
+
+
+export default Home 
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+
+    screens:{
+     
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Quicksand", "sans-serif"],
+
+      },
+
+      colors: {
+        
+      },
+      backgroundImage:{
+      }
+      
+    },
+
+    
+  },
+  plugins: [],
+}
