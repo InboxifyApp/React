cd15df198d97553cf3e5d44d901a862a0117325b
Alaa Barka <tmakaveli643@gmail.com>
2023-04-24 14:33:17 +0100

    preparing the me page

--- src/pages/me.tsx
+++ src/pages/me.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import * as Cookie from 'react-cookie'
 import useToken from '../hooks/useAuth'
 import useGetDatas from '../hooks/getUser'
+import * as SVG from './../components/svg'
+import { Link } from 'react-router-dom'
 const Me = () =>{
     const [cookie, setCookie, removeCookie] = Cookie.useCookies()
 
@@ -13,16 +15,26 @@ const Me = () =>{
     console.log("tst" , datas)
     if(datas && datas.status != 200) {
         //remove cookie 
-        //localStorage.removeItem("token")
-        //window.location.href = "/signin"
+        localStorage.removeItem("token")
+        window.location.href = "/signin"
     }
 
     console.log(datas)
     
     
         return (
-        <div>
-            <h1>heloo wol</h1>
+        <div className='w-ull h-screen bg-dark flex items-center justify-center'>
+            <aside className='w-1/4 h-full bg-purple flex items-center justify-between flex-col'>
+                <div className='w-full flex items-center '>
+                    <SVG.Logo  Width="100" />
+                    <p className='text-white text-bold font-bold pt-4'>{datas.datas.fullname}</p>
+                </div>  
+                <div className='w-full flex'>
+                    <Link to={""}className='text-white w-full py-4 bg-darg'>Home : All Clusters</Link>
+                    
+                </div>
+            </aside>
+            <aside className='w-3/4 h-full '></aside>
         </div>
         )
 }
