6cef6505d0687e3f848fa61514050126845572bd
Alaa Barka <tmakaveli643@gmail.com>
2023-05-31 03:46:49 +0100

    mapping on the fake datas

--- src/components/all.messages.tab.tsx
+++ src/components/all.messages.tab.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react'
 import SearchBox from './Search.box'
 import * as Components from './../components/components'
-
+import {messages} from './../utils/fakeDatas'
 const MessagesTab = ({...props}) =>{
     return (
-        <div className='w-full h-auto '>
+        <div className='w-full h-screen overflow-auto'>
             <SearchBox/>
                 <Components.TableRow Viewed={true}/>
-            
+                {
+                    messages.map((data:any, index:any) => {
+                        return (
+                            <Components.TableRow Viewed={data.Viewed} key={index}/>
+                        )
+                    })
+                }
 
         </div>
     )
