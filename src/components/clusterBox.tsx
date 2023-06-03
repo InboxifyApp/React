b3ce2c08fae2e34d91a8b68e129bf429bc7c137b
Alaa Barka <tmakaveli643@gmail.com>
2023-06-03 08:54:40 +0100

    cluster blocks

--- src/components/clusterBox.tsx
+++ src/components/clusterBox.tsx
@@ -1,18 +1,15 @@
+import * as React from 'react';
 
+const ClusterBox = ({ ...props }) => {
+  console.log(props.Data.color);
+  const bg = props.Data && props.Data.color ? props.Data.color : "#962134";
 
+  return (
+    <div className="w-3/12 flex justify-center items-center h-40 rounded-xl shadow-xl flex-col gap-2" style={{ backgroundColor: bg }}>
+      <p className="text-white font-bold">Cluster A</p>
+      <p className="text-white font-bold">1253 Messages</p>
+    </div>
+  );
+};
 
-import * as React from 'react'
-
-
-const ClusterBox = ({...props}) =>{
-    console.log(props.Data.color)
-    const fckingBG : any = props.Data.color as String  ? props.Data.color as String : "#962134"
-    
-    return (
-        <div className={`w-3/12  flex justify-center items-center h-40 rounded-xl shadow-xl flex-col gap-2 bg-[${fckingBG}]`}>       
-            <p className="text-white font-bold ">Cluster A</p>
-            <p className="text-white font-bold ">1253 Messages</p>
-        </div>
-    )
-}
-export default ClusterBox
+export default ClusterBox;
