17d28462080473dda326fbcb87741d3c52c7b0fa
Alaa Barka <tmakaveli643@gmail.com>
2023-06-15 07:39:37 +0100

    sender

--- src/hooks/useSender.ts
+++ src/hooks/useSender.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import * as React from 'react'
-
+import {toast} from 'react-hot-toast'
 
 
 const useSender = () =>{
@@ -23,15 +23,44 @@ const useSender = () =>{
         })
         
     }
-    return {
-        datas, Fetch
-    }
+    
+    
 
-
-    const Send = async () =>{
-
-
+    const [name, setName] = React.useState("")
+    const [email, setEmail] =React.useState("")
+    const [phone, setPhone ] = React.useState("")
+    const [message ,setMessage]= React.useState("")
+    const [subject, setSubject] = React.useState("")
+    const Send = async (id : any) =>{
         
+        const reqBody : any = {
+            name: name,
+            email: email,
+            phone: phone,
+            message: message,
+            subject: subject,
+            cluster : id
+
+        }
+
+        const target: string = `http://${import.meta.env.VITE_TARGET_BACKEND}:3001/cluster/`
+
+        return await axios.post(target,reqBody, {
+            headers:{
+                apikey : 123
+            }
+        })
+        
+    }
+    return {
+        datas, Fetch,
+
+        name, setName, 
+        email, setEmail, 
+        phone, setPhone, 
+        message, setMessage, 
+        subject, setSubject,
+        Send
     }
 }
 
--- src/pages/send.tsx
+++ src/pages/send.tsx
@@ -3,8 +3,18 @@ import * as Components from './../components/components'
 import * as ChackraUI from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
 import useSender from '../hooks/useSender'
+import { Toaster, toast } from 'react-hot-toast'
 const Send = () =>{
-    const {Fetch, datas} = useSender()
+    const {
+        datas, Fetch,
+
+        name, setName, 
+        email, setEmail, 
+        phone, setPhone, 
+        message, setMessage, 
+        subject, setSubject,
+        Send
+    } = useSender()
     const params = useParams()
     console.log(params.id)
 
@@ -34,19 +44,52 @@ const Send = () =>{
                         <p className='p-10 font-bold text-3xl' style={{
                             color : datas ? datas.color : "#fff"
                         }}>Contact {datas ? datas.name : "Loading"}</p>
-                            <ChackraUI.FormControl className="flex-col flex items-center justify-center gap-4">
-                            <Components.Input Class="w-2/5" Placeholder="Full Name" />
-                            <Components.Input Class="w-2/5" Placeholder="Email" />
-                            <Components.Input Class="w-2/5" Placeholder="Phone" />
-                            <Components.Input Class="w-2/5" Placeholder="Subject" />
+                            <ChackraUI.FormControl className="flex-col flex items-center justify-center gap-4" onSubmit={(e)=>{
+                                
+                                e.preventDefault();
+                                Send(params.id).then(res=>{
+                                    toast.success("Done Sending the Message")
+                                })
+                                .catch(e=>{
+                                    toast.error("Error Sending the Message")
+                                })
+                            }}>
+                            <Components.Input Class="w-2/5" Placeholder="Full Name" value={name}
+                                onChange={(e:any)=>{setName(e.target.value)}}
+                            />
+                            <Components.Input Class="w-2/5" Placeholder="Email" value={email} 
+                                onChange={(e:any)=>{setEmail(e.target.value)}}
+                            />
+                            <Components.Input Class="w-2/5" type="number" Placeholder="Phone" 
+                            value={phone}
+                                onChange={(e:any)=>{setPhone(e.target.value)}}
+                            />
+                            <Components.Input Class="w-2/5" Placeholder="Subject" value={subject}
+                                onChange={(e:any) =>{
+                                    setSubject(e.target.value)
+                                }}
+                            />
                             
-                            <textarea rows={"10"} className='outline-0 p-2 rounded bg-[#313131] w-2/5 text-white border border-[#666666] shadow-5xl text-sm' placeholder='Your Message'></textarea>
+                            <textarea  
+                                value={message}
+                                onChange={(e:any)=>{
+                                    setMessage(e.target.value)
+                                }
                             
-                            <button className='text-white bg-purple px-14 py-3 rounded shadow-5xl text-sm'>
+                            }
+                            rows={10} className='outline-0 p-2 rounded bg-[#313131] w-2/5 text-white border border-[#666666] shadow-5xl text-sm' placeholder='Your Message'></textarea>
+                            
+                            <button className='text-white bg-purple px-14 py-3 rounded shadow-5xl text-sm' type='submit'>
                                 Send Your Message
                             </button>
                         </ChackraUI.FormControl>
                     </div>
+
+                    <Toaster
+                        position="top-right"
+                        reverseOrder={false}
+                    />
+                    
                 </div>
             )
         
